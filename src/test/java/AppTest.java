158562368dde0d3e853b7460b3cd06d38bd47a25
Alessandro Giffra <alessandro.giffra.001@student.uni.lu>
2021-04-30 07:18:59 +0200

    Q1 to Q4

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -11,28 +11,54 @@ import org.junit.*;
 import static org.junit.Assert.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import main.java.DB;
+import main.java.App;
+import spark.Spark;
+
 public class AppTest extends FluentTest {
   public WebDriver webDriver = new HtmlUnitDriver();
 
-  @Rule
-  public DatabaseRule database = new DatabaseRule();
+  @Before
+  public void beforeEach() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/pokedex_test", "postgres", "yamizi");
+  }
+
+  @After
+  public void afterEach() {
+    try(Connection con = DB.sql2o.open()) {
+      String deletePokemonsQuery = "DELETE FROM pokemons *;";
+      String deleteMovesQuery = "DELETE FROM moves *;";
+      String deleteMovesPokemonsQuery = "DELETE FROM moves_pokemons *;";
+      con.createQuery(deletePokemonsQuery).executeUpdate();
+      con.createQuery(deleteMovesQuery).executeUpdate();
+      con.createQuery(deleteMovesPokemonsQuery).executeUpdate();
+    }
+  }
 
   @Override
   public WebDriver getDefaultDriver() {
     return webDriver;
   }
 
-  @ClassRule
-  public static ServerRule server = new ServerRule();
+  @BeforeClass
+  public static void beforeAll() {
+    String[] args = {};
+    App.main(args);
+  }
+
+  @AfterClass
+  public static void afterAll() {
+    Spark.stop();
+  }
 
   @Test
-  public void rootTest() {
+  public void acceptance_test_check_if_app_is_in_root_page() {
     goTo("http://localhost:4567/");
     assertThat(pageSource()).contains("Pokedex");
   }
 
   @Test
-  public void allPokemonPageIsDisplayed() {
+  public void acceptance_test_check_if_ivysaur_and_charizard_are_in_the_deck() {
     goTo("http://localhost:4567/");
     click("#viewDex");
     assertThat(pageSource().contains("Ivysaur"));
@@ -40,30 +66,29 @@ public class AppTest extends FluentTest {
   }
 
   @Test
-  public void individualPokemonPageIsDisplayed() {
+  public void acceptance_test_check_if_charizard_page_is_displayed() {
     goTo("http://localhost:4567/pokepage/6");
     assertThat(pageSource().contains("Charizard"));
   }
 
   @Test
-  public void arrowsCycleThroughPokedexCorrectly() {
+  public void acceptance_test_check_if_right_arrow_to_go_to_next_pokemon_page_is_working() {
     goTo("http://localhost:4567/pokepage/6");
     click(".glyphicon-triangle-right");
     assertThat(pageSource().contains("Squirtle"));
   }
 
   @Test
-  public void searchResultsReturnMatches() {
+  public void acceptance_test_check_if_search_pokemon_by_name_is_working_when_some_matches_are_found() {
     goTo("http://localhost:4567/pokedex");
     fill("#name").with("char");
     assertThat(pageSource().contains("Charizard"));
   }
 
   @Test
-  public void searchResultsReturnNoMatches() {
+  public void acceptance_test_check_if_search_pokemon_by_name_is_working_when_no_matches_are_found() {
     goTo("http://localhost:4567/pokedex");
     fill("#name").with("x");
     assertThat(pageSource().contains("No matches for your search results"));
   }
-
 }
--- src/test/java/PokemonTest.java
+++ src/test/java/PokemonTest.java
@@ -8,44 +8,64 @@ import org.sql2o.*;
 import main.java.Move;
 import main.java.Pokemon;
 
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
 public class PokemonTest {
+  private String attack;
+  private String type;
+  private double power;
+  private int accuracy;
+  private int expectedResult;
 
   @Rule
   public DatabaseRule database = new DatabaseRule();
 
+  public PokemonTest(String attack, String type, double power, int accuracy, int expectedResult) {
+    this.attack = attack;
+    this.type = type;
+    this.power = power;
+    this.accuracy = accuracy;
+    this.expectedResult = expectedResult;
+  }
+
   @Test
-  public void Pokemon_instantiatesCorrectly_true() {
+  public void unit_test_check_if_new_pokemon_is_instantiated_correctly() {
     Pokemon myPokemon = new Pokemon("Squirtle", "Water", "None", "A cute turtle", 50.0, 12, 16, false);
     assertEquals(true, myPokemon instanceof Pokemon);
   }
 
   @Test
-  public void getName_pokemonInstantiatesWithName_String() {
+  public void unit_test_check_if_new_instantiated_pokemon_has_a_name_value() {
     Pokemon myPokemon = new Pokemon("Squirtle", "Water", "None", "A cute turtle", 50.0, 12, 16, false);
     assertEquals("Squirtle", myPokemon.getName());
   }
 
   @Test
-  public void all_emptyAtFirst() {
+  public void unit_test_check_if_list_of_pokemon_is_empty_at_first() {
     assertEquals(Pokemon.all().size(), 0);
   }
 
   @Test
-  public void equals_returnsTrueIfPokemonAreTheSame_true() {
+  public void integration_test_check_if_two_pokemon_are_the_same() {
     Pokemon firstPokemon = new Pokemon("Squirtle", "Water", "None", "A cute turtle", 50.0, 12, 16, false);
     Pokemon secondPokemon = new Pokemon("Squirtle", "Water", "None", "A cute turtle", 50.0, 12, 16, false);
     assertTrue(firstPokemon.equals(secondPokemon));
   }
 
   @Test
-  public void save_savesPokemonCorrectly_1() {
+  public void integration_test_check_if_new_pokemon_is_saved_correctly() {
     Pokemon newPokemon = new Pokemon("Squirtle", "Water", "None", "A cute turtle", 50.0, 12, 16, false);
     newPokemon.save();
     assertEquals(1, Pokemon.all().size());
   }
 
   @Test
-  public void find_findsPokemonInDatabase_true() {
+  public void integration_test_check_if_a_pokemon_is_in_the_database() {
     Pokemon myPokemon = new Pokemon("Squirtle", "Water", "None", "A cute turtle", 50.0, 12, 16, false);
     myPokemon.save();
     Pokemon savedPokemon = Pokemon.find(myPokemon.getId());
@@ -53,7 +73,7 @@ public class PokemonTest {
   }
 
   @Test
-  public void addMove_addMoveToPokemon() {
+  public void integration_test_check_if_adding_a_new_move_to_a_pokemon_is_working() {
     Move myMove = new Move("Punch", "Normal", 50.0, 100);
     myMove.save();
     Pokemon myPokemon = new Pokemon("Squirtle", "Water", "None", "A cute turtle", 50.0, 12, 16, false);
@@ -64,7 +84,7 @@ public class PokemonTest {
   }
 
   @Test
-  public void delete_deleteAllPokemonAndMovesAssociations() {
+  public void integration_test_check_if_a_pokemon_is_deleted_correctly_and_that_also_associated_moves_are_deleted() {
     Pokemon myPokemon = new Pokemon("Squirtle", "Water", "None", "A cute turtle", 50.0, 12, 16, false);
     myPokemon.save();
     Move myMove = new Move("Bubble", "Water", 50.0, 100);
@@ -76,18 +96,27 @@ public class PokemonTest {
   }
 
   @Test
-  public void searchByName_findAllPokemonWithSearchInputString_List() {
+  public void integration_test_check_if_search_pokemon_by_name_is_working_if_the_input_is_a_string() {
     Pokemon myPokemon = new Pokemon("Squirtle", "Water", "None", "A cute turtle", 50.0, 12, 16, false);
     myPokemon.save();
     assertEquals(myPokemon, Pokemon.searchByName("squir").get(0));
   }
 
+  @Parameters
+  public static Collection<Object[]> values() {
+    return Arrays.asList(new Object[][] {
+      { "Atk1", "Fire", 50.0, 100, 400 }, //
+      { "Atk2", "Thunder", 50.0, 100, 300 }, //
+      { "Atk3", "Water", 20.0, 100, 460 }, //
+    });
+  }
+
   @Test
-  public void fighting_damagesDefender() {
+  public void integration_test_check_if_a_move_is_doing_the_right_amount_of_damage_to_the_targeted_pokemon() {
     Pokemon myPokemon = new Pokemon("Squirtle", "Water", "Normal", "A cute turtle", 50.0, 12, 16, false);
     myPokemon.save();
     myPokemon.hp = 500;
-    Move myMove = new Move("Bubble", "Water", 50.0, 100);
+    Move myMove = new Move(attack, type, power, accuracy);
     myMove.attack(myPokemon);
     System.out.println(myPokemon.hp);
     myMove.attack(myPokemon);
@@ -95,7 +124,6 @@ public class PokemonTest {
     myMove.attack(myPokemon);
         System.out.println(myPokemon.hp);
     myMove.attack(myPokemon);
-    assertEquals(400, myPokemon.hp);
+    assertEquals(expectedResult, myPokemon.hp);
   }
-
 }
